5fd0b66e7c3b44c90cba530e1c89a45c5e364d4b
anaknittel <62256247+anaknittel@users.noreply.github.com>
2020-10-01 20:24:15 +0100

    hooks - setState

--- src/App.js
+++ src/App.js
@@ -1,50 +1,59 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Burger from "./Burger/Burger";
 
-class App extends Component {
-  state = {
+const app = (props) => {
+  const [burgersState, setBurgersState] = useState({
     burgers: [
       { name: "BigChicken", price: 4 },
       { name: "BigFish", price: 3 },
       { name: "BigBeaf", price: 5 },
     ],
+    otherState: "null",
+  });
+
+  console.log(burgersState);
+
+  const switchBurgerHandler = () => {
+    //console.log("HEY");
+    //DONT DO THIS burgersState.burgers[0].name = 'SmallChicken'
+    setBurgersState({
+      burgers: [
+        { name: "SmallChicken", price: 2 },
+        { name: "SmallFish", price: 2.5 },
+        { name: "SmallBeaf", price: 4 },
+      ],
+    });
   };
 
-  switchBurgerHandler = () => {
-    console.log("HEY");
-  };
+  return (
+    <div className="App">
+      <h1>Burger</h1>
+      <p>Menu</p>
+      <button onClick={switchBurgerHandler}>Switch Burger's Size</button>
+      <Burger
+        name={burgersState.burgers[0].name}
+        price={burgersState.burgers[0].price}
+      />
+      <Burger
+        name={burgersState.burgers[1].name}
+        price={burgersState.burgers[1].price}
+      >
+        Extra Sauce!
+      </Burger>
+      <Burger
+        name={burgersState.burgers[2].name}
+        price={burgersState.burgers[2].price}
+      />
+    </div>
+  );
+  // return React.createElement(
+  //   "div",
+  //   { className: "App" },
+  //   React.createElement("h1", null, "Burger")
+  // );
+};
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Burger</h1>
-        <p>Menu</p>
-        <button onClick={this.switchBurgerHandler}>Switch Burger</button>
-        <Burger
-          name={this.state.burgers[0].name}
-          price={this.state.burgers[0].price}
-        />
-        <Burger
-          name={this.state.burgers[1].name}
-          price={this.state.burgers[1].price}
-        >
-          Extra Sauce!
-        </Burger>
-        <Burger
-          name={this.state.burgers[2].name}
-          price={this.state.burgers[2].price}
-        />
-      </div>
-    );
-    // return React.createElement(
-    //   "div",
-    //   { className: "App" },
-    //   React.createElement("h1", null, "Burger")
-    // );
-  }
-}
-
-export default App;
+export default app;
 
 //{this} refers to the class App

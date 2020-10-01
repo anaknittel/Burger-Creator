a4c5823fa19a6aeb0bbfbfee959fbaa11aa6f9f4
anaknittel <62256247+anaknittel@users.noreply.github.com>
2020-10-01 19:48:12 +0100

    Developing

--- src/App.js
+++ src/App.js
@@ -3,16 +3,38 @@ import "./App.css";
 import Burger from "./Burger/Burger";
 
 class App extends Component {
+  state = {
+    burgers: [
+      { name: "BigChicken", price: 4 },
+      { name: "BigFish", price: 3 },
+      { name: "BigBeaf", price: 5 },
+    ],
+  };
+
+  switchBurgerHandler = () => {
+    console.log("HEY");
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Burger</h1>
         <p>Menu</p>
-        <Burger name="BigChicken" price="4" />
-        <Burger name="BigFish" price="3">
+        <button onClick={this.switchBurgerHandler}>Switch Burger</button>
+        <Burger
+          name={this.state.burgers[0].name}
+          price={this.state.burgers[0].price}
+        />
+        <Burger
+          name={this.state.burgers[1].name}
+          price={this.state.burgers[1].price}
+        >
           Extra Sauce!
         </Burger>
-        <Burger name="BigBeaf" price="5" />
+        <Burger
+          name={this.state.burgers[2].name}
+          price={this.state.burgers[2].price}
+        />
       </div>
     );
     // return React.createElement(
@@ -24,3 +46,5 @@ class App extends Component {
 }
 
 export default App;
+
+//{this} refers to the class App

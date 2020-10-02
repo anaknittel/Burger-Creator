f1e2730201c3a5b49efcabf225a0dd7a65c708dc
anaknittel <62256247+anaknittel@users.noreply.github.com>
2020-10-02 16:31:22 +0100

    getting started with styling

--- src/Burger/Burger.js
+++ src/Burger/Burger.js
@@ -7,7 +7,7 @@ const burger = (props) => {
         {props.name} - {props.price} Euros!
       </p>
       <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name} />
+      <input type="text" onChange={props.changed} />
     </div>
   );
 };
@@ -17,3 +17,5 @@ export default burger;
 //const as is not changeble. const is a function with arrow syntx
 // () = argument
 //props.children can be anything in between opened and closed objects: list, text, ...
+
+// <input type="text" onChange={props.changed} value={props.name} />  makes the input is filled with the names before changing
